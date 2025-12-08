3d29dccaad739325cc096ca60d96e1ca384ab032
Yusuf <yusufer.net@hotmail.com>
2025-12-08 17:32:51 +0200

    Initial commit

---
+++ libs/Api.py
@@ -0,0 +1,58 @@
+import requests
+
+class Api:
+    BASE_URL = "https://automationexercise.com/api" # base url of the api we will be calling(used for all endpoints)
+
+
+    def get_all_products(self) -> requests.Response:
+        url = f"{self.BASE_URL}/productsList"   # productsList endpoint returns all products
+
+        #send a get request to retrieve product data from the server
+        #get means w request data only ! WE DO NOT MODIFY,DELETE,ADD ANYTHING!!
+        return requests.get(url) # return the server's raw HTTTP response to be valdiated later.
+
+    def get_all_products_should_return_200(self) -> dict: #main test validation function = returns parsed json
+
+        response = self.get_all_products()  # wil contatin status code + headers + body
+
+        #validate that the status code is 200 (success)
+        #if it is not 200,the test must fail because the api did not respond succesfully
+
+        if response.status_code !=200: #raise assertionerror stops the test and prints a clear failure message
+            raise AssertionError(
+                f"expected status 200, got {response.status_code}. Body:  {response.text}"
+            ) #assertionerror means expected condition did NOT met ->test fail
+
+        # Try to convert the response body into JSON format
+        # If this fails, the server returned invalid data (not JSON)
+        try:
+            data = response.json()  # Convert the raw response text into Python dictionary
+        except ValueError:  # Raised when JSON decoding fails
+                raise AssertionError(f"Invalid JSON. Body: {response.text}")  # Fail if JSON is broken
+
+        # Check if the JSON contains the required key "products"
+        # This is essential because we expect the product list to be inside this key
+        if "products" not in data:
+            raise AssertionError(f"'products' key missing. JSON: {data}")
+
+        # Acces the first product in the list to be inspect its structure and confirm valid content
+        first = data["products"][0]
+
+        # Validate that the first product contains required fields: id, name, price
+        # These keys must exist - otherwise the API data is incomplete and incorrect
+
+        for key in ("id", "name", "price"):
+            if key not in first:
+                raise AssertionError(
+                    f"Missing key '{key}' in first product. Product: {first}"
+                )
+
+        return data # All checks passed -> return the JSON data back to Robot Framework for additional verification
+
+
+
+
+
+
+
+
---
+++ tests/API_1.robot
@@ -0,0 +1,10 @@
+*** Settings ***
+Library     ../libs/Api.py
+
+*** Test Cases ***
+API 1: Get All Products List
+    [Tags]    regression    api
+    ${result}=    Get All Products Should Return 200
+    Should Not Be Empty    ${result}
+
+
